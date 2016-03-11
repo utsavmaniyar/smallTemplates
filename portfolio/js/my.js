43dfa3694caa231bc10b7769f184eb7b1b59b581
utsavmaniyar <utsavmaniyar@gmail.com>
2016-03-11 16:37:41 -0500

    portfolio Added
    This is sample portfolio with all required files.

---
+++ portfolio/js/my.js
@@ -0,0 +1,15 @@
+console.log('hi');
+function openWin(img){
+
+           var path = "img-port/";
+           document.getElementsByClassName("portfolioImg")[0].src = path+img;
+       }
+
+/*function toggle_visibility(id) {
+	console.log('hi');
+			       var e = document.getElementById(id);
+			       if(e.style.display == 'block')
+			          e.style.display = 'none';
+			       else
+			          e.style.display = 'block';
+			    }*/
---
+++ portfolio/my.js
@@ -0,0 +1,6 @@
+
+function my(){
+	myWindow.document.write("<img alt='ALTERNATIVE' src='i2.png' />");
+}
+
+
